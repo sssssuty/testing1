d254abe1c61c836c46a469dad8a5c9492ace6108
Tingyu Su <tingyusu@tingyus-mbp.wireless.newschool.edu>
2020-02-12 17:19:41 -0500

    Feb12

--- codemain.js
+++ codemain.js
@@ -5,68 +5,103 @@ function myFunction() {
     // console.log(filter);
     if (filter =="ALLWORK"){
         console.log(filter);
+        document.getElementById("companyname").innerHTML = "ALLWORK";
         $(".inputbar").css({"display": "none"});
         $(".project").css({"display": "grid"});
     }
 
     if (filter =="THEMET"){
         console.log(filter);
+        document.getElementById("companyname").innerHTML = "creative team from the MET";
+        $("#fnamecopy1").css({"display": "block"});
+        $("#fnamecopy2").css({"display": "block"});
+        $(".inputbar").css({"display": "none"});
+        $(".end").css({"display": "block"});
+
         $("#displacement").css({"display": "grid"});
-        $("#lacma").css({"display": "grid"});
         $("#guggenheim").css({"display": "grid"});
         $("#rubin").css({"display": "grid"});
-        $("#mswho").css({"display": "grid"});
+        $("#lacma").css({"display": "grid"});
+        $("#jinhe").css({"display": "grid"});
         $("#proxi").css({"display": "grid"});
+        $("#mswho").css({"display": "grid"});
     }
 
     if (filter =="MOMA"){
         console.log(filter);
+        document.getElementById("companyname").innerHTML = "MoMA creative team";
+        $("#fnamecopy1").css({"display": "block"});
+        $("#fnamecopy2").css({"display": "block"});
+        $(".inputbar").css({"display": "none"});
+        $(".end").css({"display": "block"});
+
         $("#displacement").css({"display": "grid"});
-        $("#lacma").css({"display": "grid"});
         $("#guggenheim").css({"display": "grid"});
         $("#rubin").css({"display": "grid"});
-        $("#mswho").css({"display": "grid"});
+        $("#lacma").css({"display": "grid"});
+        $("#jinhe").css({"display": "grid"});
         $("#proxi").css({"display": "grid"});
+        $("#mswho").css({"display": "grid"});
     }
 
     if (filter =="GOOGLE"){
         console.log(filter);
+        document.getElementById("companyname").innerHTML = "Google creative team";
         $(".inputbar").css({"display": "none"});
+        $("#fnamecopy1").css({"display": "block"});
+        $("#fnamecopy2").css({"display": "block"});
+        $(".end").css({"display": "block"});
+
         $("#lacma").css({"display": "grid"});
         $("#nad").css({"display": "grid"});
         $("#experienceyelp").css({"display": "grid"});
         $("#wulala").css({"display": "grid"});
         $("#letterc").css({"display": "grid"});
-        $("#randomcontrol").css({"display": "grid"});
+        // $("#randomcontrol").css({"display": "grid"});
     }
 
     if (filter =="DROPBOX"){
         console.log(filter);
+        document.getElementById("companyname").innerHTML = "Dropbox digital product team";
         $(".inputbar").css({"display": "none"});
+        $("#fnamecopy1").css({"display": "block"});
+        $("#fnamecopy2").css({"display": "block"});
+        $(".end").css({"display": "block"});
+
         $("#lacma").css({"display": "grid"});
         $("#experienceyelp").css({"display": "grid"});
         $("#wulala").css({"display": "grid"});
         $("#letterc").css({"display": "grid"});
-        $("#randomcontrol").css({"display": "grid"});
+        // $("#randomcontrol").css({"display": "grid"});
     }
 
     if (filter =="ADOBE"){
         console.log(filter);
+        document.getElementById("companyname").innerHTML = "Adobe design team";
         $(".inputbar").css({"display": "none"});
+        $("#fnamecopy1").css({"display": "block"});
+        $("#fnamecopy2").css({"display": "block"});
+        $(".end").css({"display": "block"});
+
         $("#lacma").css({"display": "grid"});
         $("#experienceyelp").css({"display": "grid"});
         $("#wulala").css({"display": "grid"});
         $("#letterc").css({"display": "grid"});
-        $("#randomcontrol").css({"display": "grid"});
+        // $("#randomcontrol").css({"display": "grid"});
     }
 
     if (filter =="FACEBOOK"){
         console.log(filter);
+        document.getElementById("companyname").innerHTML = "Facebook digital product team";
         $(".inputbar").css({"display": "none"});
+        $("#fnamecopy1").css({"display": "block"});
+        $("#fnamecopy2").css({"display": "block"});
+        $(".end").css({"display": "block"});
+
         $("#lacma").css({"display": "grid"});
         $("#experienceyelp").css({"display": "grid"});
         $("#wulala").css({"display": "grid"});
         $("#letterc").css({"display": "grid"});
-        $("#randomcontrol").css({"display": "grid"});
+        // $("#randomcontrol").css({"display": "grid"});
     }
 }
--- indexmain.js
+++ indexmain.js
@@ -32,6 +32,6 @@ $( document ).ready(function() {
         // console.log(num);
         num++;
 
-    }, 3000);
+    }, 1500);
 
 });
