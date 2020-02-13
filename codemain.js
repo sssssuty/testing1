1e4c09909b2d95659e924c16ff6f1ccc083217ff
Tingyu Su <tingyusu@Tingyus-MBP.fios-router.home>
2020-02-13 00:34:27 -0500

    Feb13

--- codemain.js
+++ codemain.js
@@ -13,8 +13,8 @@ function myFunction() {
     if (filter =="THEMET"){
         console.log(filter);
         document.getElementById("companyname").innerHTML = "creative team from the MET";
-        $("#fnamecopy1").css({"display": "block"});
-        $("#fnamecopy2").css({"display": "block"});
+        // $("#fnamecopy1").css({"display": "block"});
+        // $("#fnamecopy2").css({"display": "block"});
         $(".inputbar").css({"display": "none"});
         $(".end").css({"display": "block"});
 
@@ -30,8 +30,8 @@ function myFunction() {
     if (filter =="MOMA"){
         console.log(filter);
         document.getElementById("companyname").innerHTML = "MoMA creative team";
-        $("#fnamecopy1").css({"display": "block"});
-        $("#fnamecopy2").css({"display": "block"});
+        // $("#fnamecopy1").css({"display": "block"});
+        // $("#fnamecopy2").css({"display": "block"});
         $(".inputbar").css({"display": "none"});
         $(".end").css({"display": "block"});
 
@@ -48,8 +48,8 @@ function myFunction() {
         console.log(filter);
         document.getElementById("companyname").innerHTML = "Google creative team";
         $(".inputbar").css({"display": "none"});
-        $("#fnamecopy1").css({"display": "block"});
-        $("#fnamecopy2").css({"display": "block"});
+        // $("#fnamecopy1").css({"display": "block"});
+        // $("#fnamecopy2").css({"display": "block"});
         $(".end").css({"display": "block"});
 
         $("#lacma").css({"display": "grid"});
@@ -64,8 +64,8 @@ function myFunction() {
         console.log(filter);
         document.getElementById("companyname").innerHTML = "Dropbox digital product team";
         $(".inputbar").css({"display": "none"});
-        $("#fnamecopy1").css({"display": "block"});
-        $("#fnamecopy2").css({"display": "block"});
+        // $("#fnamecopy1").css({"display": "block"});
+        // $("#fnamecopy2").css({"display": "block"});
         $(".end").css({"display": "block"});
 
         $("#lacma").css({"display": "grid"});
@@ -79,8 +79,8 @@ function myFunction() {
         console.log(filter);
         document.getElementById("companyname").innerHTML = "Adobe design team";
         $(".inputbar").css({"display": "none"});
-        $("#fnamecopy1").css({"display": "block"});
-        $("#fnamecopy2").css({"display": "block"});
+        // $("#fnamecopy1").css({"display": "block"});
+        // $("#fnamecopy2").css({"display": "block"});
         $(".end").css({"display": "block"});
 
         $("#lacma").css({"display": "grid"});
@@ -94,8 +94,8 @@ function myFunction() {
         console.log(filter);
         document.getElementById("companyname").innerHTML = "Facebook digital product team";
         $(".inputbar").css({"display": "none"});
-        $("#fnamecopy1").css({"display": "block"});
-        $("#fnamecopy2").css({"display": "block"});
+        // $("#fnamecopy1").css({"display": "block"});
+        // $("#fnamecopy2").css({"display": "block"});
         $(".end").css({"display": "block"});
 
         $("#lacma").css({"display": "grid"});
@@ -103,5 +103,56 @@ function myFunction() {
         $("#wulala").css({"display": "grid"});
         $("#letterc").css({"display": "grid"});
         // $("#randomcontrol").css({"display": "grid"});
+    }
+
+    if (filter =="IBM"){
+        console.log(filter);
+        document.getElementById("companyname").innerHTML = "IBM";
+        $(".inputbar").css({"display": "none"});
+        // $("#fnamecopy1").css({"display": "block"});
+        // $("#fnamecopy2").css({"display": "block"});
+        $(".end").css({"display": "block"});
+
+        $("#lacma").css({"display": "grid"});
+        $("#experienceyelp").css({"display": "grid"});
+        $("#wulala").css({"display": "grid"});
+        $("#letterc").css({"display": "grid"});
+        $("#proxi").css({"display": "grid"});
+        // $("#randomcontrol").css({"display": "grid"});
+    }
+
+    if (filter =="CHANEL"){
+        console.log(filter);
+        document.getElementById("companyname").innerHTML = "Chanel";
+        $(".inputbar").css({"display": "none"});
+        // $("#fnamecopy1").css({"display": "block"});
+        // $("#fnamecopy2").css({"display": "block"});
+        $(".end").css({"display": "block"});
+
+        $("#mswho").css({"display": "grid"});
+        $("#Amantes").css({"display": "grid"});
+        $("#guggenheim").css({"display": "grid"});
+        $("#lacma").css({"display": "grid"});
+        $("#proxi").css({"display": "grid"});
+        // $("#randomcontrol").css({"display": "grid"});
+    }
+
+    if (filter =="PROPHET"){
+        console.log(filter);
+        document.getElementById("companyname").innerHTML = "Chanel";
+        $(".inputbar").css({"display": "none"});
+        // $("#fnamecopy1").css({"display": "block"});
+        // $("#fnamecopy2").css({"display": "block"});
+        $(".end").css({"display": "block"});
+
+        $("#mswho").css({"display": "grid"});
+        $("#Amantes").css({"display": "grid"});
+        $("#guggenheim").css({"display": "grid"});
+        $("#lacma").css({"display": "grid"});
+        $("#proxi").css({"display": "grid"});
+        $("#nad").css({"display": "grid"});
+        $("#theheaven").css({"display": "grid"});
+        // $("#displacement").css({"display": "grid"});
+        // $("#jinhe").css({"display": "grid"});
     }
 }
