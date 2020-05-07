af7dca8ac9739e21fbf7df98ad8f85b92aaf429c
Tingyu Su <tingyusu@Tingyus-MBP.fios-router.home>
2020-05-06 23:19:26 -0400

    May6

--- indexmain.js
+++ indexmain.js
@@ -1,38 +1,35 @@
-$( document ).ready(function() {
+// $( document ).ready(function() {
 
-    var elem = document.getElementById("moving1");
-    var num = 0;
-    // var num = setInterval(frame, 5);
-    // console.log(num);
+//     var elem = document.getElementById("moving1");
+//     var num = 0;
 
-    setInterval(function(){
-        if (num == 0){
-        elem.innerHTML = "·loves to hear from the team and contribute mine·";
-        };
+//     setInterval(function(){
+//         if (num == 0){
+//         elem.innerHTML = "·loves to hear from the team and contribute mine·";
+//         };
 
-        if (num == 1) {
-        elem.innerHTML = "·values ideation, process and final execution·";
-        };
+//         if (num == 1) {
+//         elem.innerHTML = "·values ideation, process and final execution·";
+//         };
 
-        if (num == 2) {
-        elem.innerHTML = "·enjoys storytelling with various formats and mediums·";
-        };
+//         if (num == 2) {
+//         elem.innerHTML = "·enjoys storytelling with various formats and mediums·";
+//         };
 
-        if (num == 3) {
-        elem.innerHTML = "·wants to know about you and perhaps, work with you :)";
-        };
+//         if (num == 3) {
+//         elem.innerHTML = "·wants to know about you and perhaps, work with you :)";
+//         };
 
-        if (num == 4) {
-        elem.innerHTML = "·believes design has the power to make a difference·";
-        num = -1;
-        };
+//         if (num == 4) {
+//         elem.innerHTML = "·believes design has the power to make a difference·";
+//         num = -1;
+//         };
 
-        // console.log(num);
-        num++;
+//         num++;
 
-    }, 2400);
+//     }, 2400);
 
-});
+// });
 
 function startTime() {
   var today = new Date();
