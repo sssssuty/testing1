c819277c250b35ca4a2f19e4de53db33012b27d6
sssssuty <43247180+sssssuty@users.noreply.github.com>
2023-07-13 14:26:33 -0400

    July13

--- passcode.js
+++ passcode.js
@@ -2,7 +2,10 @@ function myFunction() {
     var fname = document.getElementById("fname").value;
     var passcode = fname.toUpperCase();
     if (passcode == "FACEBOOK"){
-      window.open("https://www.sutingyu.com/facebook.html", "_self");
+      window.open("https://www.sutingyu.com/facebook/index.html", "_self");
+    }
+    if (passcode == "CMU"){
+      window.open("https://www.sutingyu.com/cmu/index.html", "_self");
     }
     else{
       document.getElementById("noti").style.display = "inline-block";
