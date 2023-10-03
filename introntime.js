2f8d081d37384b488b833f551facd86d064884ba
sssssuty <43247180+sssssuty@users.noreply.github.com>
2023-10-02 22:30:23 -0400

    Oct2

--- introntime.js
+++ introntime.js
@@ -1,36 +1,3 @@
-$( document ).ready(function() {
-
-    var elem = document.getElementById("moving1");
-    var num = 0;
-
-    setInterval(function(){
-        if (num == 0){
-        elem.innerHTML = "speaks English, Mandarin, and code.";
-        };
-
-        if (num == 1) {
-        elem.innerHTML = "values ideation, process and execution.";
-        };
-
-        if (num == 2) {
-        elem.innerHTML = "tells stories with various mediums.";
-        };
-
-        if (num == 3) {
-        elem.innerHTML = "loves to work in a team and collaborate.";
-        };
-
-        if (num == 4) {
-        elem.innerHTML = "has four years of experience.";
-        num = -1;
-        };
-
-        num++;
-
-    }, 2000);
-
-});
-
 function startTime() {
   var today = new Date();
   var h = today.getHours();
