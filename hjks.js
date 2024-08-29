9e352b42ecc0f45a25db415da5891418911c57ee
sssssuty <sssssuty@gmail.com>
2024-08-29 09:22:41 -0400

    updated the site structure

--- hjks.js
+++ hjks.js
@@ -2,10 +2,16 @@ function myFunction() {
     var fname = document.getElementById("fname").value;
     var passcode = fname.toUpperCase();
     if (passcode == "FRONTEND"){
-      window.open("https://www.sutingyu.com/frontend/index.html", "_self");
+      window.open("https://www.sutingyu.com/work/frontend/index.html", "_self");
     }
     if (passcode == "DESIGN24"){
-      window.open("https://www.sutingyu.com/design/index.html", "_self");
+      window.open("https://www.sutingyu.com/work/productdesign/index.html", "_self");
+    }
+    if (passcode == "PRODUCT"){
+      window.open("https://www.sutingyu.com/work/product/index.html", "_self");
+    }
+    if (passcode == "DESIGNWORK"){
+      window.open("https://www.sutingyu.com/work/design/index.html", "_self");
     }
     if (passcode == "SELFF"){
       window.open("www.google.com", "_self");
