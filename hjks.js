3918ea1fc0aa6f3b137754230b2205c17a515808
sssssuty <sssssuty@gmail.com>
2024-12-13 13:22:00 -0500

    Adding AMEX page

--- hjks.js
+++ hjks.js
@@ -16,6 +16,9 @@ function myFunction() {
     if (passcode == "DESIGNENGINEER"){
       window.open("https://www.sutingyu.com/work/designengineer/index.html", "_self");
     }
+    if (passcode == "AMEX"){
+      window.open("https://www.sutingyu.com/work/amex/index.html", "_self");
+    }
     // if (passcode == "SELFF"){
     //   window.open("www.google.com", "_self");
     // }
