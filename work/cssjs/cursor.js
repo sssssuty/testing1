580bf5673edbef6249dcb4190b23b0b8c15e356e
sssssuty <sssssuty@gmail.com>
2026-02-09 14:02:43 -0800

    feb9

--- work/cssjs/cursor.js
+++
@@ -1,6 +0,0 @@
-const bodyElement = document.body;
-
-bodyElement.addEventListener('mousemove', e => {
-  bodyElement.style.setProperty('--pageX', e.pageX + 'px');
-  bodyElement.style.setProperty('--pageY', e.pageY + 'px');
-});
